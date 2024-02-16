6b225f6b0c9d86f8eb51e7388237a06c8c7827b2
WooDoo-1 <antongavrilicev903@gmail.com>
2024-02-16 21:15:18 +0300

    Потоки, практика. Thread.sleep(); volatile;

--- src/test/practice/Test.java
+++ src/test/practice/Test.java
@@ -1,10 +1,44 @@
 package test.practice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Обратный отсчет
+*/
+
 public class Test {
+    public static volatile List<String> list = new ArrayList<>(5);
+
+    static {
+        for (int i = 0; i < 5; i++) {
+            list.add("Строка " + i);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
+        Thread t = new Thread(new Countdown(3),"Countdown");
+        t.start();
+    }
+    public static class Countdown implements Runnable {
+        private int countFrom;
+
+        public Countdown(int countFrom) {
+            this.countFrom = countFrom;
+        }
+        public void run() {
+            try {
+                while (countFrom >= 0) {
+                    printCountdown();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        public void printCountdown() throws InterruptedException {
+                Thread.sleep(500);
+
+                System.out.println(list.get(countFrom));
+                countFrom--;
+        }
     }
 }
