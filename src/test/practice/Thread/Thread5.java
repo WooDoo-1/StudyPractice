49bc5e2dde91bbaf8984cf5f47fc41ffbbd19be0
WooDoo-1 <antongavrilicev903@gmail.com>
2024-02-17 17:25:27 +0300

    volatile практика.

---
+++ src/test/practice/Thread/Thread5.java
@@ -0,0 +1,28 @@
+package test.practice.Thread;
+
+public class Thread5 {
+    public static volatile boolean finish = false;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        finish = true;
+    }
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (!finish) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
