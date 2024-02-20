6c2e0baca5baa75fbad21466db2246f7ba5ad135
WooDoo-1 <antongavrilicev903@gmail.com>
2024-02-20 18:02:53 +0300

    Singleton паттерн - синхронизация в методе

---
+++ src/test/practice/Thread/task5/IMF.java
@@ -0,0 +1,13 @@
+package test.practice.Thread.task5;
+
+public class IMF {
+
+    private static IMF imf;
+
+    public static IMF getFund() {
+        synchronized (IMF.class) {if (imf == null) {imf = new IMF();}}
+        return imf;
+    }
+    private IMF() {
+    }
+}
---
+++ src/test/practice/Thread/task5/Solution.java
@@ -0,0 +1,15 @@
+package test.practice.Thread.task5;
+
+/*
+МВФ
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        IMF fund = IMF.getFund();
+        IMF anotherFund = IMF.getFund();
+        System.out.println(fund == anotherFund);
+    }
+}
+
+
